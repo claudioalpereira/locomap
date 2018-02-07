e2146806a00365fa280f1cdc54b878621b5f2f41
Pereira <claudio.pereira@siemens.com>
2018-02-07 15:12:44 +0000

    TEMP prettyfied register page, so the user can choose her password

--- sieIFM/Controllers/ManageController.cs
+++ sieIFM/Controllers/ManageController.cs
@@ -237,7 +237,8 @@ namespace MVCSignalRtest2.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                 }
-                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                //return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                return RedirectToAction("Index", "Home");
             }
             AddErrors(result);
             return View(model);
